cc38ad8d0d9c0f881250b11c1d8b3e93b8a6275e
Lucas Medeiros <lucasmedeiros1994@gmail.com>
2020-06-05 02:03:09 -0300

    Start frontend

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles';
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home;
